6d25c20a2958480c91bcd47e5343fdb5d95bf57b
fferreira12 <fferreira12@gmail.com>
2019-09-29 18:38:57 -0300

    Adds filters to balance list component

--- src/app/accounting/balance-list/balance-list.component.ts
+++ src/app/accounting/balance-list/balance-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { AccountingService } from "@core/services/accounting.service";
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: "app-balance-list",
@@ -8,6 +9,12 @@ import { AccountingService } from "@core/services/accounting.service";
 })
 export class BalanceListComponent implements OnInit {
   balances: { account: string; balance: number }[] = [];
+  filteredBalances: { account: string; balance: number }[] = [];
+
+  filters = new FormGroup({
+    hideZeros: new FormControl(true),
+    accName: new FormControl(''),
+  });
 
   constructor(private accountingService: AccountingService) {}
 
@@ -19,7 +26,10 @@ export class BalanceListComponent implements OnInit {
     this.accountingService.subscribeToAccount(accs => {
       this.updateBalances(this.accountingService.getBalances());
     });
-
+    //filter form
+    this.filters.valueChanges.subscribe(val => {
+      this.updateFilteredBalances(val);
+    });
   }
 
   updateBalances(balances: { [s: string]: number }) {
@@ -28,5 +38,25 @@ export class BalanceListComponent implements OnInit {
     for (var key in balances) {
       this.balances.push({ account: key, balance: balances[key] });
     }
+    this.updateFilteredBalances();
+  }
+
+  updateFilteredBalances(val: {hideZeros:boolean, accName: string} = this.filters.value) {
+    if(!val) {
+      this.filteredBalances = this.balances;
+    }
+    this.filteredBalances = this.balances.filter(balance => {
+      if(val.accName && val.accName !== "" && val.hideZeros) {
+        return balance.account.indexOf(val.accName) >= 0 && Math.abs(balance.balance) > 0.01;
+      }
+      if(val.accName && val.accName !== "") {
+        return balance.account.indexOf(val.accName) >= 0;
+      }
+      if(val.hideZeros) {
+        return Math.abs(balance.balance) > 0.01;
+      }
+      return true;
+    })
+
   }
 }
